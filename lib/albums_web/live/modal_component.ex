dbe8f9387959f057e8847756c040ef8fb22be1c0
Max Mahmoud Wardeh <m@lurnid.com>
2021-08-21 23:00:06 +0100

    Use bootstrap modal for live component forms

--- lib/albums_web/live/modal_component.ex
+++ lib/albums_web/live/modal_component.ex
@@ -4,16 +4,20 @@ defmodule AlbumsWeb.ModalComponent do
   @impl true
   def render(assigns) do
     ~L"""
-    <div id="<%= @id %>" class="phx-modal"
+    <div id="<%= @id %>" class="live-modal fade" tabindex="-1"
       phx-capture-click="close"
       phx-window-keydown="close"
       phx-key="escape"
-      phx-target="#<%= @id %>"
+      phx-target="#<%= @myself %>"
       phx-page-loading>
 
-      <div class="phx-modal-content">
-        <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
-        <%= live_component @socket, @component, @opts %>
+      <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+          <%= live_patch "", to: @return_to, class: "btn-close",  aria_label: "Close" %>
+          <div class="modal-body">
+            <%= live_component @socket, @component, @opts %>
+          </div>
+        </div>
       </div>
     </div>
     """
