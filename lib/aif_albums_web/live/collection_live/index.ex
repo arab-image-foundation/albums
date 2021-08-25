2c18fdb16050ac949f0ebda64a153ed31bbc6c85
Max Mahmoud Wardeh <m@lurnid.com>
2021-08-25 19:04:35 +0100

    Update collections listing page

--- lib/aif_albums_web/live/collection_live/index.ex
+++ lib/aif_albums_web/live/collection_live/index.ex
@@ -2,45 +2,38 @@ defmodule AIFAlbumsWeb.CollectionLive.Index do
   use AIFAlbumsWeb, :live_view
 
   alias AIFAlbums.Collections
-  alias AIFAlbums.Collections.Collection
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :collections, list_collections())}
+    socket =
+      socket
+      |> assign(
+        temporary_assign: [
+          collections: []
+        ]
+      )
+      |> assign(page_title: "AIF Albums")
+      |> assign(sort_by: :aifid)
+      |> assign(sort_order: :asc)
+
+    {:ok, socket}
   end
 
   @impl true
-  def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  def handle_params(_params, _url, socket) do
+    collections =
+      list_collections(
+        sort: %{sort_by: socket.assigns.sort_by, sort_order: socket.assigns.sort_order}
+      )
+
+    socket =
+      socket
+      |> assign(collections: collections)
+
+    {:noreply, socket}
   end
 
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Collection")
-    |> assign(:collection, Collections.get_collection!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Collection")
-    |> assign(:collection, %Collection{})
-  end
-
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "Listing Collections")
-    |> assign(:collection, nil)
-  end
-
-  @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    collection = Collections.get_collection!(id)
-    {:ok, _} = Collections.delete_collection(collection)
-
-    {:noreply, assign(socket, :collections, list_collections())}
-  end
-
-  defp list_collections do
-    Collections.list_collections()
+  defp list_collections(criteria) do
+    Collections.list_collections(criteria)
   end
 end
