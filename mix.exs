316fa3ef6c1e5ac0f7eecd9ff0bac8c826ef5b4d
Max Mahmoud Wardeh <m@lurnid.com>
2021-08-26 01:16:34 +0100

    Add Timex package

--- mix.exs
+++ mix.exs
@@ -46,6 +46,7 @@ defmodule AIFAlbums.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:timex, "~> 3.0"},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
